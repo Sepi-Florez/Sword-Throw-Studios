014ffed0fe30368aff2c2475a844918f4563a7f0
David Vink <david.vink@hotmail.com>
2016-06-09 21:22:17 +0200

    Added work to portal scripts

---
+++ Assets/Scripts/General/Portal.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Portal : MonoBehaviour {
+	public string destination;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	void OnCollisionEnter(Collision entered){
+		if(entered.transform.tag == "Player" ){
+			Application.LoadLevel(destination);
+		}
+	}
+}
