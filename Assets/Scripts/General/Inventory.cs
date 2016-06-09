309ccb3479b84d3008ad1f92006e7d8397caa6a3
David Vink <david.vink@hotmail.com>
2016-06-09 19:50:10 +0200

    Bezig geweest met Inventory System

--- Assets/Scripts/General/Inventory.cs
+++ Assets/Scripts/General/Inventory.cs
@@ -14,6 +14,7 @@ public class Inventory : MonoBehaviour {
 		for(int a = 0; a < invObj.transform.GetChild(0).childCount; a++){
 			invSlots.Add(invObj.transform.GetChild(0).GetChild(a).GetComponent<Button>());
 		}
+		invSlots[0].onClick.AddListener(() => Move1(item));
 	}
 	void Update () {
 		if(follow == true){
@@ -25,22 +26,25 @@ public class Inventory : MonoBehaviour {
 	}
 	public void Move1 (Transform imag){
 		if(follow == false){
-			follow = true;
 			item = imag;
-
+			follow = true;
+			print("Grabed Item");		
+			invSlots[0].onClick.RemoveListener(() => Move1(item));
 		}
 		else{
-			
 			follow = false;
-			
+			print("putdown Item");
 		}
 	}
 	public void Move2 (int but){
 		if(follow == false){
-			
+			//invSlots[but].onClick.RemoveListener(() => Move1(item));
+			print("deleted listener");
 		}
 		else{
+			//invSlots[but].onClick.AddListener(() => Move1(item));
 			item.position = invSlots[but].transform.position;
+			print("new listener");
 		}
 		
 	}
