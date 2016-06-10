661dd992a384573b2e92576513dd47a8a1ffca78
David Vink <david.vink@hotmail.com>
2016-06-10 23:36:54 +0200

    Started work on Flux movement
    Hovering toegevoegd. Look at WIP

---
+++ Assets/Scripts/Npc's/Fluxs.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Fluxs : MonoBehaviour {
+	public float amplitude;
+	public float speed;
+	
+	public Transform standardFocus;
+	Transform newFocus;
+	Vector3 flux;
+	// Use this for initialization
+	void Start () {
+		flux = transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		hover();
+		Focus(newFocus);
+		
+	}
+	void hover(){
+		transform.position = flux;
+		flux.y += amplitude * Mathf.Sin(speed * Time.time);
+	}
+	void Focus (Transform focus){
+		transform.LookAt(focus);
+	}
+	void OnTriggerEnter (Collider entered) {
+		if(entered.transform.tag == "Player"){
+			newFocus = entered.transform;
+		}
+	}
+	void OnTriggerExit (Collider left) {
+		if(left.transform.tag == "Player"){
+			newFocus = standardFocus;
+		}
+	}
+}
