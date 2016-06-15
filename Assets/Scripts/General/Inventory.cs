b0f4bd013f74ddd6c11dbd8da9dda58059dc04f1
David Vink <david.vink@hotmail.com>
2016-06-15 08:45:55 +0200

    Some Script Edits

--- Assets/Scripts/General/Inventory.cs
+++ Assets/Scripts/General/Inventory.cs
@@ -7,18 +7,18 @@ public class Inventory : MonoBehaviour {
 	public List<Button> invSlots = new List<Button>();
 	
 	public GameObject invObj;
-	public Transform item;
+	public Transform[] item;
 	
 	public bool follow;
 	void Start () {
 		for(int a = 0; a < invObj.transform.GetChild(0).childCount; a++){
 			invSlots.Add(invObj.transform.GetChild(0).GetChild(a).GetComponent<Button>());
 		}
-		invSlots[0].onClick.AddListener(() => Move1(item));
+		invSlots[0].onClick.AddListener(() => Move1(item[1]));
 	}
 	void Update () {
 		if(follow == true){
-			item.position = Input.mousePosition;
+			item[1].position = Input.mousePosition;
 		}
 	}
 	public void toggle () {
@@ -26,25 +26,24 @@ public class Inventory : MonoBehaviour {
 	}
 	public void Move1 (Transform imag){
 		if(follow == false){
-			item = imag;
+			item[0] = imag;
+			//invSlots[0].onClick.RemoveListener(() => Move1(item));
 			follow = true;
 			print("Grabed Item");		
-			invSlots[0].onClick.RemoveListener(() => Move1(item));
-		}
-		else{
-			follow = false;
-			print("putdown Item");
+			
 		}
 	}
 	public void Move2 (int but){
 		if(follow == false){
-			//invSlots[but].onClick.RemoveListener(() => Move1(item));
+			invSlots[but].onClick.RemoveListener(() => Move1(item[1]));
 			print("deleted listener");
 		}
 		else{
-			invSlots[but].onClick.AddListener(() => Move1(item));
-			item.position = invSlots[but].transform.position;
+			invSlots[but].onClick.AddListener(() => Move1(item[1]));
+			item[1].position = invSlots[but].transform.position;
 			print("new listener");
+			follow = false;
+			print("putdown Item");
 		}
 		
 	}
