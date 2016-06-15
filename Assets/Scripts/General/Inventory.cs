ac1d8853690cec03bdace08cfdbd1d60c045e7c3
David Vink <david.vink@hotmail.com>
2016-06-15 13:24:33 +0200

    Pre Pause Edits

--- Assets/Scripts/General/Inventory.cs
+++ Assets/Scripts/General/Inventory.cs
@@ -6,6 +6,7 @@ using UnityEngine.UI;
 public class Inventory : MonoBehaviour {
 	public List<Button> invSlots = new List<Button>();
 	public List<Transform> items = new List<Transform>();
+	public Transform heldItem;
 	public int slotNumber;
 	
 	public GameObject invObj;
@@ -14,7 +15,9 @@ public class Inventory : MonoBehaviour {
 	void Start () {
 		for(int a = 0; a < invObj.transform.GetChild(0).childCount; a++){
 			invSlots.Add(invObj.transform.GetChild(0).GetChild(a).GetComponent<Button>());
-			items[slotNumber] = invSlots[a].transform.GetChild(0);
+			if(invSlots[a].transform.childCount != 0){
+				items.Add (invSlots[a].transform.GetChild(0).transform);
+			}
 		}
 		invSlots[0].onClick.AddListener(() => Move1(items[slotNumber]));
 	}
@@ -29,22 +32,22 @@ public class Inventory : MonoBehaviour {
 	public void Move1 (Transform imag){
 		print("move1 activated");
 		if(follow == false){
-			items[slotNumber] = imag;
+			heldItem = imag;
 			follow = true;
 			print("Grabed Items");		
 			
 		}
 	}
 	public void Move2 (int but){
-		print("move2 activated");
 		slotNumber = but;
 		if(follow == false){
 			invSlots[but].onClick.RemoveAllListeners();
 			print("deleted listener");
 		}
 		else{
-			invSlots[but].onClick.AddListener(() => Move1(items[but]));
-			items[but].position = invSlots[but].transform.position;
+			invSlots[but].onClick.AddListener(() => Move1(heldItem));
+			heldItem.position = invSlots[but].transform.position;
+			heldItem.SetParent(invSlots[but].transform);
 			print("new listener");
 			follow = false;
 			print("putdown Items");
