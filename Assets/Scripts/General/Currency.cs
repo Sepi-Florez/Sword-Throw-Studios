dea69d9503ddeafcf4d5d3edcba11343387ae3b2
David Vink <david.vink@hotmail.com>
2016-06-21 21:56:45 +0200

    Currency Edits
    UI works and added a "Obtained within a short time" counter to the UI.
    All works

--- Assets/Scripts/General/Currency.cs
+++ Assets/Scripts/General/Currency.cs
@@ -1,15 +1,32 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class Currency : MonoBehaviour {
 
 	public int soulCount;
+	public int soulsObtained;
 
 	public int minValue;
 	public int maxValue;
 
+	public GameObject soulCountUI;
+	public GameObject soulsObtainedUI;
+	Text soulsObtainedText;
+	Text soulCountText;
+	public float soulsObtainedTime;
+
+	bool gathering;
+	float timer;
+
+	void Start () {
+		soulsObtainedText = soulsObtainedUI.transform.GetComponent<Text>();
+		soulCountText = soulCountUI.transform.GetComponent<Text>();
+	}
+
 	void Update () {
 		InterFace ();
+		Timer();
 	}
 	void OnTriggerEnter (Collider soul) {
 		switch(soul.transform.tag){
@@ -23,9 +40,26 @@ public class Currency : MonoBehaviour {
 	}
 	void pickUp (int value) {
 		soulCount += value;
+		soulsObtained += value;
+		timer = 0;
 		print(soulCount);
+		gathering = true;
+		soulsObtainedUI.SetActive(true);
 	}
 	void InterFace () {
-
+		soulCountText.text = soulCount.ToString();
+		soulsObtainedText.text = soulsObtained.ToString();
+	}
+	void Timer () {
+		if(gathering == true){
+			timer += Time.deltaTime;
+			if(timer >= soulsObtainedTime){
+				gathering = false;
+			}
+		}
+		else{
+			soulsObtainedUI.SetActive(false);
+			soulsObtained = 0;
+		}
 	}
 }
